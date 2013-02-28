e912afe1762e8692d02260f2a19b61ef169eb59b
Ciaran Roarty <ciaran.roarty@gmail.com>
2013-02-28 20:50:26 +0000

    Start again

--- Browser Automation Helper/Atos.BrowserAutomationHelper.Library/WebTest.Partial.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Atos.BrowserAutomationHelper.Library
-{
-    /// <summary>
-    /// This class is the base class for all Web Tests - it contains the helper methods used by all Browser Automation Web Tests
-    /// </summary>
-    public abstract partial class WebTest
-    {
-    }
-}
--- Browser Automation Helper/Atos.BrowserAutomationHelper.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Atos.BrowserAutomationHelper.Web.MvcApplication" Language="C#" %>
